0b0c077f60c785c60a32ce951c9946fd3334d740
satnick <satish64607@gmail.com>
2016-07-06 14:54:29 +0530

    Session_3_Assignment_1

---
+++ SqrAndCubeRoot.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+public class SqrAndCubeRoot{
+
+     public static void main(String []args){
+        System.out.println(Math.sqrt(9));
+        System.out.println(Math.cbrt(27));
+        
+        System.out.println("Hello World");
+     }
+}
