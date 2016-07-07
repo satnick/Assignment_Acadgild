a12ff2c17a93b56e1bb7695ffb355e63deba8f72
satnick <satish64607@gmail.com>
2016-07-07 16:06:57 +0530

    Session_4_Assignment_3

---
+++ Employee.java
@@ -0,0 +1,49 @@
+class Employee
+{
+	private int empID;
+	private String empName;
+	private String joiningDate;
+	private double salary;
+	
+	
+	public void setEmpID(int id)
+	{
+		empID = id;
+	}
+	
+	public int getEmpID()
+	{
+		return empID;
+	}
+	
+	public void setEmpName(String name)
+	{
+		empName = name;
+	}
+	
+	public String getEmpName()
+	{
+		return empName;
+	}
+	
+	public void setJoiningDate(String date)
+	{
+		joiningDate = date;
+	}
+	
+	public String getJoiningDate()
+	{
+		return joiningDate;
+	}
+	
+	public void setSalary(double sal)
+	{
+		salary = sal;
+	}
+	
+	public int getSalary()
+	{
+		return salary;
+	}
+
+}
